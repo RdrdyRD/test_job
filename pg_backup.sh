93524d599d010ff1000f8d9b37a35f7842e2487d
RdrdyRD <hotcalorie2023@gmail.com>
2025-10-05 11:03:07 +0700

    Create pg_backup.sh

---
+++ pg_backup.sh
@@ -0,0 +1,196 @@
+#!/bin/bash
+
+set -euo pipefail 
+IFS=$'\n\t' 
+
+#Логирование
+LOG_FILE="/var/log/backup.log"
+BACKUP_DIR="/backups"
+TIMESTAMP=$(date '+%Y%m%d_%H%M%S')
+BACKUP_NAME="backup_${TIMESTAMP}.gz"
+TEMP_DIR=$(mktemp -d) 
+
+   
+#Постгре настройки
+
+PG_HOST="localhost"
+PG_PORT="5432"
+PG_USER="postgres"
+
+
+
+
+#функция логгирования:
+
+log() {
+    local level=$1
+    shift 
+    local message=$*
+    echo "[$(date "+%Y-%m-%d %H:%M:%S')] [$level] $message" | tee -a "$LOG_FILE"  
+}
+
+log_info() {
+    log "INFO" "$@"
+}
+
+log_error () {
+    log "ERROR" "$@"
+}
+
+log_success() {
+    log "SUCCESS" "$@"
+}
+
+#функция очистки
+
+cleanup() {
+    local exit_code=$?
+    
+    log_info "Очистка временных файлов!"
+    
+    if [[ -d "$TEMP_DIR" ]]; then
+        rm -rf "$TEMP_DIR"
+        log_success "Временная директория удалена: $TEMP_DIR"
+    fi
+           
+   
+    if [[ $exit_code -ne 0 ]]; then
+        log_error "Ошибка во время выполнения скрипта: $exit_code"
+        exit $exit_code
+    fi
+}
+
+trap cleanup EXIT
+
+#функции проверок
+
+check_dependencies() {
+    log_info "Проверка наличия зависимостей..."
+    local missing=()
+    local dependencies=("gzip" "pg_dump" "psql")
+    
+    for dep in "${dependencies[@]}"; do
+        if ! command -v "$dep" &>/dev/null; then
+            missing+=("$dep")
+            log_error "Не найдена утилита $dep"
+        fi
+    done    
+    
+    if (( ${#missing[@]} > 0 )); then
+        log_error "Отсутствуют зависимости: ${missing[*]}"
+        exit 1
+    else 
+        log_success "Все зависимости установлены!" 
+    fi
+}
+
+check_diskspace() {
+    log_info "Проверка наличия необходимого свободного места в $BACKUP_DIR !"
+    local dir_space
+    dir_space=$(df "$BACKUP_DIR" | awk "NR==2 {print \$4}")
+    
+    #1 гигабайт минимум
+    if [[ $dir_space -lt 1048576 ]]; then
+        log_error "В $BACKUP_DIR не хватает свободного места! Сейчас там $dir_space KB"
+        exit 1
+    fi
+    
+    log_success "В $BACKUP_DIR достаточно свободного места: $dir_space KB!"
+}
+
+#основная логика бэкапа
+
+main() {
+    log_info "Приступаем к резервному копированию!"
+    
+    check_dependencies
+    check_diskspace
+    mkdir -p "$BACKUP_DIR"
+    
+    log_info "Получение списка баз данных!"
+    local databases
+    if ! databases=$(psql -h "$PG_HOST" -p "$PG_PORT" -U "$PG_USER" -d postgres -t -c "
+        SELECT datname FROM pg_database
+        WHERE datistemplate = false
+        AND datname NOT IN ('postgres')
+        order by datname;" 2>&1); then
+        log_error "Ошибка при исполнении запроса psql: $databases"
+        exit 1
+    fi
+    
+    if [[ -z "$databases" ]]; then
+        log_error "Ошибка получения списка датабаз или список пуст!"
+        exit 1
+    fi
+    
+    log_success "Датабазы найдены: $(echo "$databases" | tr '\n' ' ')" 
+    
+    local dump_errors=0
+    
+    for db in $databases; do
+        log_info "Создание дампа для датабазы: $db"
+        local dump_file="${TEMP_DIR}/${db}.sql"
+        if pg_dump -h "$PG_HOST" -p "$PG_PORT" -U "$PG_USER" -d "$db" -f "$dump_file" 2>>"$LOG_FILE"; then
+            log_success "Дамп датабазы $db успешно создан!"
+            if [[ ! -s "$dump_file" ]]; then
+                log_error "Дамп датабазы $db НЕ создан или пуст!"
+                ((dump_errors++))
+            fi    
+        else
+            log_error "Ошибка при создании дампа $db"
+            ((dump_errors++))
+        fi    
+    done
+
+    if [[ $dump_errors -gt 0 ]]; then
+        log_error "Ошибки при создании дампов датабазы. Всего $dump_errors ошибок!"
+        exit 1
+    fi    
+
+#создание архива
+
+    log_info "Создание архива $BACKUP_NAME"
+    
+    if cd "$TEMP_DIR" && tar -czf "${TEMP_DIR}/${BACKUP_NAME}" *.sql 2>>"$LOG_FILE"; then
+        log_success "Архив ${TEMP_DIR}/${BACKUP_NAME} успешно создан!"
+    else
+        log_error "Ошибка при создании архива ${TEMP_DIR}/${BACKUP_NAME}"
+    	exit 1
+    fi    
+    
+#проверка валидности архива
+
+    log_info "Проверка целостности архива ${TEMP_DIR}/${BACKUP_NAME}"
+        if gzip -t "${TEMP_DIR}/${BACKUP_NAME}" 2>>"$LOG_FILE"; then
+            log_success "Архив прошел проверку целостности!"
+        else
+            log_error "Архив НЕ прошёл проверку целостности!"
+            exit 1
+        fi    
+
+#перемещение архива в целевую директорию
+
+    log_info "Перемещение ${TEMP_DIR}/${BACKUP_NAME} в $BACKUP_DIR"
+        if mv "${TEMP_DIR}/${BACKUP_NAME}" "${BACKUP_DIR}/${BACKUP_NAME}"; then
+            log_success "Архив успешно перемещен ${BACKUP_DIR}/${BACKUP_NAME}!"
+        else
+            log_error "Произошла ошибка при перемещении архива в ${BACKUP_DIR}/${BACKUP_NAME}"
+            exit 1
+        fi    
+       
+#финальная проверка
+
+    local final_size
+    final_size=$(du -h "${BACKUP_DIR}/${BACKUP_NAME}" | cut -f1)
+    
+    log_success "Резервное копирование успешно произведено!"
+    log_success "Архив: ${BACKUP_DIR}/${BACKUP_NAME}" 
+    log_success "Размер: $final_size KB"
+}
+
+if [[ $EUID -eq 0 ]]; then
+    log_error "Ошибка: нельзя запускать от root. Используйте пользователя postgres!"
+    exit 1
+fi    
+
+main
